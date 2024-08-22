2aae5cd321167c488dca5ebbc069f7340aad81f3
IT24101447 <it24101447@my.sliit.lk>
2024-08-22 11:11:45 +0530

    ADD IT24101447Lab4Q1A.java To Q1

---
+++ Q1/IT24101447Lab4Q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class IT24101447Lab4Q1 { 
+public static void main(String[] args) {
+	int num;
+	Scanner input=new Scanner(System.in);
+	
+	System.out.print("Enter a number: ");
+	num=input.nextInt();
+	if(num==0){
+		System.out.println("The number is : zero");
+		}
+	else if (num>0){
+		System.out.println("The number is : Positive");
+	}
+	else {
+		System.out.println("The number is: Negative");
+	}
+}
+
+}
